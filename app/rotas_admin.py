4821d59390b35466abc88d1a3305d0d43e467aa0
javisgameacademy01 <javisgameacademy01@gmail.com>
2026-02-20 16:26:06 -0300

    Update rotas_admin.py

--- app/rotas_admin.py
+++ app/rotas_admin.py
@@ -2,7 +2,7 @@
 Rotas administrativas do sistema
 """
 import os
-from fastapi import APIRouter, HTTPException, Header, UploadFile, File, Form
+from fastapi import APIRouter, HTTPException, Header, UploadFile, File, Form, Query
 from pydantic import BaseModel
 from supabase import create_client, Client
 from datetime import datetime, timedelta
